4a85d839a6d7efacd1d462858cc05d2c47073e88
nikitaselivyorstov <nikitaseliverstov@gmail.com>
2020-12-14 00:42:10 +0300

    Initial commit

--- Find the Album App/App Delegate/SceneDelegate.swift
+++ Find the Album App/App Delegate/SceneDelegate.swift
@@ -17,6 +17,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.rootViewController?.view.backgroundColor = .yellow
+        window?.makeKeyAndVisible()
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
