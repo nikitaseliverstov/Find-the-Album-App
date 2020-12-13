8a77d34cbc041b453e520df230188e3967f1520e
nikitaselivyorstov <nikitaseliverstov@gmail.com>
2020-12-14 00:28:42 +0300

    Initial Commit

---
+++ Find the Album App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Find the Album App
+//
+//  Created by Nikita Seliverstov on 14.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
